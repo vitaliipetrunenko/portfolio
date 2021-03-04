db480a287a722f08bd42a07ee940c4055b70e2cf
Vitalii Petrunenko <vitaliipetrunenko@gmail.com>
2021-03-04 20:27:03 +0200

    added css module

--- src/components/header/__buttonPlatform/header__buttonPlatform.jsx
+++ src/components/header/__buttonPlatform/header__buttonPlatform.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import './header__buttonPlatform.css';
+import styl from'./header__buttonPlatform.module.css';
 import Style from 'style-it';
 
 function Header__buttonPlatform(props) {
-    return  (<div className='Header__buttonPlatform' href="#">
+    return  (<div className={styl.Header__buttonPlatform} href="#">
             
-        <a className="Header__button_hover">{props.txt}</a>
-        <a className="Header__button_unhover">{props.txt}</a>
+        <a className={styl.Header__button_hover}>{props.txt}</a>
+        <a className={styl.Header__button_unhover}>{props.txt}</a>
 
         </div>);
+
     
 }
 
